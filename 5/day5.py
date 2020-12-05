7957a2ea5eede5d501f76b14c705d831c53e06a3
samira keshavarz <s.keshavarz@gmx.de>
2020-12-05 22:02:36 +0100

    improve the code

--- 5/day5.py
+++ 5/day5.py
@@ -7,39 +7,35 @@ import readFile
 
 FILE = "input.txt"
 #FILE = "test.txt"
+MIN_ROW = 0
+MAX_ROW = 127
+MIN_COLUMN = 0
+MAX_COLUMN = 7
 
 input = readFile.readFile(FILE)
 seatIds : List[int] = []
 
+def processStr(input: str, min: int, max: int, upper: str, lower: str) -> int:
+    number : int = 0
+    for r in input[:-1]:
+        if r == lower:
+            max = math.floor((max + min) / 2)
+        if r == upper:
+            min = math.ceil((max + min) / 2)
+    if input[-1] == lower:
+        number = min
+    if input[-1] == upper:
+        number = max
+    return number
+
+
 for i in input:
-    MIN_ROW = 0
-    MAX_ROW = 127
-    MIN_COLUMN = 0
-    MAX_COLUMN = 7
-    row = i[:6]
-    for r in row:
-        if r == 'F':
-            MIN_ROW = MIN_ROW
-            MAX_ROW = math.floor((MAX_ROW + MIN_ROW) / 2)
-        if r == 'B':
-            MIN_ROW = math.ceil((MAX_ROW + MIN_ROW) / 2)
-            MAX_ROW = MAX_ROW
-    if i[6] == 'F':
-        row = MIN_ROW
-    if i[6] == 'B':
-        row = MAX_ROW
+    row = i[:7]
     column = i[7:]
-    for c in column[:-1]:
-        if c == 'L':
-            MIN_COLUMN = MIN_COLUMN
-            MAX_COLUMN = math.floor((MAX_COLUMN+MIN_COLUMN)/2)
-        if c == 'R':
-            MIN_COLUMN = math.ceil((MAX_COLUMN + MIN_COLUMN) /2)
-            MAX_COLUMN = MAX_COLUMN
-    if i[9] == 'L':
-        column = MIN_COLUMN
-    if i[9] == 'R':
-        column = MAX_COLUMN
+
+    row = processStr(row, MIN_ROW, MAX_ROW, upper = 'B', lower = 'F')
+    column = processStr(column, MIN_COLUMN, MAX_COLUMN, upper = 'R', lower = 'L')
+
     seatId = (row * 8) + column
     seatIds.append(seatId)
 
@@ -49,8 +45,8 @@ seatIds.sort()
 mySeatId = sum(range(seatIds[0],seatIds[-1]+1)) - sum(seatIds)
 
 
-
-print("############## Day 4 ##############")
+print()
+print("######## Day 4 #########")
 print()
 print("Part1: Max Seat ID: ", maxSeatId)
 print()
